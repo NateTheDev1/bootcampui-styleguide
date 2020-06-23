cd2d0af7c3996940a0e0f8ae498795b18f234008
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-23 13:48:39 -0400

    BootcampUI Styleguide v1

---
+++ precache-manifest.5b5df57931a0c01bff9e49473d84cefe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a129300ea9bac31c9f3",
+    "url": "build/2.7a129300.js"
+  },
+  {
+    "revision": "499dfcf091329244539925dd3c0ed2d1",
+    "url": "build/2.7a129300.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46008162eaa4d8d14fe",
+    "url": "build/bundle.e4600816.js"
+  },
+  {
+    "revision": "c364e8ec12c8fe4b4932",
+    "url": "build/main.c364e8ec.js"
+  },
+  {
+    "revision": "bcda3484cdedce442faad604184e756d",
+    "url": "index.html"
+  }
+]);
